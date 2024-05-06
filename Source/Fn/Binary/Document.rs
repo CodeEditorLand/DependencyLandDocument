68aa8598937febfc42b04f7874aba1cdfe4aa294
Nikola Hristov <nikola@playform.cloud>
2024-05-06 20:41:38 +0300

    squash!

--- Source/Fn/Binary/Document.rs
+++ Source/Fn/Binary/Document.rs
@@ -6,7 +6,7 @@ use Library::Fn::Cache;
 fn main() {
 	let matches = Command::new("📄 Document.")
 		.version(env!("CARGO_PKG_VERSION"))
-		.author("Nikola R. Hristov")
+		.author("Nikola Hristov")
 		.about("Build.")
 		.get_matches();
 }
